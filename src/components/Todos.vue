77185d898bcd9a28ab316a50d40b0ac09de37221
sabosan <sabosan331@gmail.com>
2020-01-09 22:46:13 +0900

    git kraken

---
+++ src/components/Todos.vue
@@ -0,0 +1,26 @@
+<template>
+
+    <div>
+        <div v-bind:key="todo.id" v-for="todo in todos">
+            <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo',todo.id)"/>
+            <!-- <h3>{{todo.title}}</h3> -->
+        </div>
+    </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue'
+
+export default {
+    name : "Todos",
+    props : ["todos"], // App.vueでlink
+    components: {
+        TodoItem
+    },
+}
+</script>
+
+<style scoped>
+
+
+</style>
